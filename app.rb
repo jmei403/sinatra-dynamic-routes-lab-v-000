8394200f984d71ca453a551f4623782ff3388c04
Jason Mei <jason.mei403@gmail.com>
2019-01-19 01:42:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,6 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get 'reversename/:name' do
-    @name = params[:name]
-    "#{@name}".reverse
+    "Hello world"
   end
 end
